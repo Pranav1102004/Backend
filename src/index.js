de03bfb92824680d1e38d6d8e79f732f9a046905
Pranav1102004 <prs11102004@gmail.com>
2024-10-15 10:32:51 +0530

    util discussion

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import connectDb from "./db/index.js";
 import path from "path";
+import { app } from "./app.js";
 
 dotenv.config({
     path : './env'
 })
 
 connectDb()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server running at port ${process.env.PORT}`);
+    });
+})
+.catch((error)=>{
+    console.log(`database connection failed!! ${error}`);
+})
